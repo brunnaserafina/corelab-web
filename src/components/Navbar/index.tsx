2a446cfbeca70718075c5f8c79ea3d14eabb8383
Brunna Serafina <brunnaserafina@gmail.com>
2023-09-30 21:37:45 -0300

    feat: added option to collapse navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,29 +1,44 @@
 import logo from "../../assets/images/logo.svg";
 import iconOut from "../../assets/images/icon-out.svg";
+import iconArrow from "../../assets/images/icon-arrow.svg";
 import Search from "../Search";
 import styles from "./Navbar.module.scss";
 import { useState } from "react";
 
-export default function Navbar() {
+export default function Navbar(props: any) {
   const [search, setSearch] = useState<string>();
+  const [showNavbar, setShowNavbar] = useState<boolean>(true);
 
   return (
-    <nav className={styles.Navbar}>
-      <div>
-        <img src={logo} alt="Logo Corelab" />
+    <>
+      {showNavbar ? (
+        <nav className={styles.Navbar}>
+          <div>
+            <img src={logo} alt="Logo Corelab" />
 
-        <p>CoreNotes</p>
+            <p>CoreNotes</p>
 
-        <Search
-          placeholder="Pesquisar notas"
-          value={search}
-          onChange={(e) => {
-            setSearch(e.target.value);
-          }}
-        />
-      </div>
+            <Search
+              placeholder="Pesquisar notas"
+              value={search}
+              onChange={(e) => {
+                setSearch(e.target.value);
+              }}
+            />
+          </div>
 
-      <img src={iconOut} alt="Ícone fechar" className={styles.Icon} />
-    </nav>
+          <img
+            src={iconOut}
+            alt="Ícone fechar"
+            className={styles.Icon}
+            onClick={() => setShowNavbar(false)}
+          />
+        </nav>
+      ) : (
+        <div className={styles.Logo} onClick={() => setShowNavbar(true)}>
+          <img src={iconArrow} alt="Logo Corelab" />
+        </div>
+      )}
+    </>
   );
 }
